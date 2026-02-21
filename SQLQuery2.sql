8f0e2fb70e254b28976906b996b255c97d291ea7
Anuj Khare <07anujkhare@gmail.com>
2026-02-21 22:44:32 +0530

    Add files via upload

---
+++ SQLQuery2.sql
@@ -0,0 +1,20 @@
+ select distinct country from customers
+
+
+ select top 3 
+ * from customers
+ order by score desc
+
+  select top 2
+ * from customers
+ order by score asc
+
+
+ select top 2 * from 
+ orders
+ order by order_date desc
+
+ select * from customers
+
+ select * from orders
+
