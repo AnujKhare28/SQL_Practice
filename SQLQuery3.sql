23f28d53807f1ca3c6d6fc0339abc987befcce3e
Anuj Khare <07anujkhare@gmail.com>
2026-02-22 23:23:24 +0530

    Add files via upload

---
+++ SQLQuery3.sql
@@ -0,0 +1,11 @@
+create table persons ( 
+	id int not null,
+	person_name varchar(50) not null,
+	birth_date date,
+	phone varchar(15) NOT NULL,
+
+	CONSTRAINT PK_PERSONS PRIMARY KEY(ID)
+)
+
+ select * from persons
+
