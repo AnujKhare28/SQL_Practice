311e388e7c47dc41f819bffd6eb227e671497aaa
Anuj Khare <07anujkhare@gmail.com>
2026-05-11 19:39:09 +0530

    Add files via upload

---
+++ 1_SELECT_practice.sql
@@ -0,0 +1,11 @@
+use mydatabase;
+
+-- 1. SELECT QUERY: 
+-- Retrieve all customer data
+select * from customers;
+
+-- Retrieve all order data
+select * from orders;
+
+-- Retrieve each customer's name, country and score
+select first_name, country, score from customers;
