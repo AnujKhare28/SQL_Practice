080f94a65f2b539276c6aae7bdf51738e8a503fa
Anuj Khare <07anujkhare@gmail.com>
2026-05-28 22:45:31 +0530

    Add files via upload

---
+++ SQLQuery85.sql
@@ -0,0 +1,23 @@
+
+Create View Sales.V_Order_Details as (
+	Select 
+		o.OrderID,
+		o.OrderDate,
+		p.Product,
+		p.Category,
+		coalesce(c.FirstName,'') + ' '+ coalesce(c.LastName, '') CustomerName,
+		c.Country CustomerCountry,
+		coalesce(e.FirstName,'') + ' '+ coalesce(e.LastName, '') SalesName,
+		e.Department,
+		o.Sales,
+		o.Quantity
+	From Sales.Orders o
+	left join Sales.Products p
+	on p.ProductID = o.ProductID
+	left join Sales.Customers c
+	on c.CustomerID = o.CustomerID
+	left join Sales.Employees e
+	on e.EmployeeID = o.SalesPersonID
+);
+
+Select * From Sales.V_Order_Details;
